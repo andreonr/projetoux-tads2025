1e39ac81d206eba98d3576efc989d9c03b57cf80
Andre <fcoandre2@gmail.com>
2025-06-24 21:36:51 -0300

    php and contact

---
+++ cardapio.php
@@ -0,0 +1,64 @@
+<?php
+
+$cardapio = [
+    [
+        'title' => 'X-Burguer',
+        'image' => 'imagens/xburguer.png',
+        'description' => 'Pão brioche, carne, queijo cheddar, alface, tomate, maionese.',
+        'price' => '25,00'
+    ],
+    [
+        'title' => 'X-Egg',
+        'image' => 'imagens/xegg.png',
+        'description' => 'Pão De Brioche, Hamburguer Bovino 180g, Mussarela, Tomate, Alface, Cebola Roxa E Ovo.',
+        'price' => '33,00'
+    ],
+    [
+        'title' => 'X-Salada',
+        'image' => 'imagens/xsalada.png',
+        'description' => 'Pão De Brioche, Hamburguer Bovino 180g, Mussarela, Tomate, Alface, Cebola Roxa.',
+        'price' => '29,90'
+    ],
+    [
+        'title' => 'X-Calabresa',
+        'image' => 'imagens/xcalabresa.png',
+        'description' => 'Pão De Brioche, Hamburguer Bovino 180g, Mussarela, Tomate, Alface, Cebola Roxa E Calabresa.',
+        'price' => '29,90'
+    ],
+    [
+        'title' => 'Duplo Cheddar',
+        'image' => 'imagens/cheddarduplo.png',
+        'description' => 'Pão De Brioche, 2 Hambuguer Bovino 180g, Mussarela, Bacon E Cheddar.',
+        'price' => '38,00' // Placeholder price, was missing in HTML
+    ],
+    [
+        'title' => 'X-Bacon',
+        'image' => 'imagens/xbacon.png',
+        'description' => 'Pão De Brioche, Hamburguer Bovino 180g, Mussarela, Tomate, Alface, Cebola Roxa E Bacon.',
+        'price' => '35,00' // Placeholder price, was missing in HTML
+    ],
+    [
+        'title' => 'Smash Cheddar Duas Camadas',
+        'image' => 'imagens/smashduplo.png',
+        'description' => 'Imagine dois hambúrguer no estilo smash, com 100g de carne cada até alcançar aquela crosta dourada irresistível. Por cima, uma generosa camada de cheddar cremoso, derretido perfeitamente para envolver cada pedaço. Para completar, fatias crocantes de bacon, que trazem um toque defumado e uma textura incrível ao lanche. Tudo isso, harmonizado dentro de um pão macio e levemente tostado, criando uma combinação perfeita de sabores e texturas que fazem cada mordida inesquecível.',
+        'price' => '45,00' // Placeholder price, was missing in HTML
+    ],
+    [
+        'title' => 'Dois X-Burguer\'s + Coca-Cola 1L',
+        'image' => 'imagens/combo1lanche.png',
+        'description' => 'Dois deliciosos X-burguers, recheados com ingredientes frescos e irresistíveis, acompanhados por uma Coca-Cola geladinha de 1 litro. Perfeito para matar a fome e a sede com muito sabor!',
+        'price' => '19,90'
+    ],
+    [
+        'title' => 'X-Bacon + X-Salada + Coca-Cola 1L',
+        'image' => 'imagens/combolanche.png',
+        'description' => 'Um suculento X-Bacon, com seu toque defumado e irresistível, acompanhado por um clássico X-Salada fresquinho e uma Coca-Cola bem gelada de 1 litro para completar a experiência. A combinação perfeita para quem ama variedade e sabor!',
+        'price' => '73,00'
+    ]
+];
+
+// You can now use this $cardapio array in your PHP file
+// For example, to print it:
+// print_r($cardapio);
+
+?>
